7bb19b7dce087cf06d7c46114ab16d7f5d77fe02
William Mabotja <57864049+williammabotja@users.noreply.github.com>
2020-02-03 08:14:40 +0200

    About Page
    Custom Routing in Express

--- Hello_Express.js
+++ Hello_Express.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send("Hello, World!");
 });
 
+app.get('/about', (req, res) => {
+    res.send("More information");
+});
+
 app.listen(80, ()=> {
     console.log("Listening on Port 80");
 });
